62c9aa0345e50e30bb4508055f672f6b7a08eb3d
Leszek <leszek.walszewski@gmail.com>
2025-10-13 22:15:32 +0200

    migration fix

--- database/migrations/landlord/0000_01_01_000000_create_workshops_table.php
+++ database/migrations/landlord/0000_01_01_000000_create_workshops_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('workshops', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('domain')->nullable()->unique();
             $table->timestamps();
             $table->softDeletes();
         });
