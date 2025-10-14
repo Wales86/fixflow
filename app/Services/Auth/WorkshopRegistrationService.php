10f0afb49a735b4f45c6e700d7429839ffab05d5
Leszek <leszek.walszewski@gmail.com>
2025-10-14 20:17:02 +0200

    added rules & little fixes

--- app/Services/Auth/WorkshopRegistrationService.php
+++ app/Services/Auth/WorkshopRegistrationService.php
@@ -13,18 +13,15 @@ class WorkshopRegistrationService
     public function register(RegisterWorkshopData $data): User
     {
         return DB::transaction(static function () use ($data) {
-            // Create workshop (tenant)
+
             $workshop = Workshop::create([
                 'name' => $data->workshop_name,
             ]);
 
-            // Make this workshop the current tenant
             $workshop->makeCurrent();
 
-            // Ensure the Owner role exists (it should be created during seeding)
             $ownerRole = Role::firstOrCreate(['name' => 'Owner']);
 
-            // Create the owner user
             $user = User::create([
                 'workshop_id' => $workshop->id,
                 'name' => $data->owner_name,
@@ -32,7 +29,6 @@ class WorkshopRegistrationService
                 'password' => $data->password,
             ]);
 
-            // Assign Owner role to the user
             $user->assignRole($ownerRole);
 
             return $user;
