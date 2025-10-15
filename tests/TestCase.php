653d975bc3bd6b8dd0c7492b33e44d65c98eb274
Leszek <leszek.walszewski@gmail.com>
2025-10-15 20:27:57 +0200

    tests fix for IDE

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,10 @@
 
 namespace Tests;
 
+use App\Models\Workshop;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public Workshop $workshop;
 }
